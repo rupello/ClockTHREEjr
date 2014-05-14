3d7fee1183309f30ce28183c78f334d90a90d66d
Rupert Lloyd <rupert.lloyd@gmail.com>
2014-05-13 22:23:02 -0400

    locate wtf from utl

--- web/clockthree.py
+++ web/clockthree.py
@@ -1,8 +1,8 @@
-
-from flask import Flask, render_template, jsonify
+import os
+from flask import Flask, render_template, abort
 import Simulate
 import clockwords
-from clockface import build_cells, english_v3
+from clockface import build_cells
 from string import lower,upper
 
 
@@ -22,12 +22,37 @@ def index():
 # TODO add dynamic clock face generation
 # TODO add lang selection eg /clockthreejr/English_v3/
 
+def findwtf(style):
+    for dirpath,dirnames,fnames in os.walk('./langs'):
+        for f in fnames:
+            name,ext = os.path.splitext(f)
+            if ext.lower()=='.wtf':
+                if style.lower()==name.lower():
+                    return os.path.join(dirpath,f)
 
-@app.route('/map')
-def map():
-    data = Simulate.readwtf('langs/English_v3.wtf')
-    return clockwords.data2json(data)
 
+@app.route('/clock3jr/<style>/map')
+def map(style):
+    wtfpath = findwtf(style)
+    if wtfpath is not None:
+        data = Simulate.readwtf(wtfpath)
+        return clockwords.data2json(data)
+    else:
+        abort(404)
+
+
+@app.route('/clock3jr/<style>/')
+def clock3jr(style):
+    wtfpath = findwtf(style)
+    if wtfpath is not None:
+        data = Simulate.readwtf(wtfpath)
+        return render_template('clock.html',
+                               cells=build_cells(fontpath=r"./fonts/JosefinSans-Regular.ttf",
+                                                 fontsize=35,
+                                                 style=data['letters'],
+                                                 case=lower))
+    else:
+        abort(404)
 
 #!flask/bin/python
 if __name__ == '__main__':
